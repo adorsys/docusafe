8741b770283af5b36e65b60afe6a59249fe8313d
pspex <psp@adorsys.de>
2019-07-01 18:13:58 +0200

    spring test for annotation

--- docusafe-spring/src/test/java/de/adorsys/docusafe/spring/SpringConfigTest.java
+++ docusafe-spring/src/test/java/de/adorsys/docusafe/spring/SpringConfigTest.java
@@ -9,7 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest(classes=SpringConfigTest.class)
+@SpringBootTest
 @RunWith(SpringRunner.class)
 @UseDocusafeSpringConfiguration
 public class SpringConfigTest {
