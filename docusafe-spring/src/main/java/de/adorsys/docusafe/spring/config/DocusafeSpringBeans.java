8742c09dbc5b765895633511511fefcd03f1ef4c
psp <psp@adorsys.de>
2019-07-04 08:05:04 +0200

    DOC-193 junit test for docusafe-spring

--- docusafe-spring/src/main/java/de/adorsys/docusafe/spring/config/DocusafeSpringBeans.java
+++ docusafe-spring/src/main/java/de/adorsys/docusafe/spring/config/DocusafeSpringBeans.java
@@ -1,18 +1,26 @@
 package de.adorsys.docusafe.spring.config;
 
 import de.adorsys.dfs.connection.api.service.api.DFSConnection;
+import de.adorsys.docusafe.business.DocumentSafeService;
+import de.adorsys.docusafe.business.impl.DocumentSafeServiceImpl;
 import de.adorsys.docusafe.cached.transactional.CachedTransactionalDocumentSafeService;
 import de.adorsys.docusafe.spring.factory.SpringCachedTransactionalDocusafeServiceFactory;
 import de.adorsys.docusafe.spring.factory.SpringDFSConnectionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
  * Created by peter on 02.10.18.
  */
+@EnableConfigurationProperties({
+
+        SpringFilesystemConnectionProperties.class,
+        SpringAmazonS3ConnectionProperties.class}
+)
 @Configuration
 public class DocusafeSpringBeans {
     private final static Logger LOGGER = LoggerFactory.getLogger(DocusafeSpringBeans.class);
@@ -23,7 +31,7 @@ public class DocusafeSpringBeans {
 
     @Bean
     public DFSConnection DFSConnection(SpringDFSConnectionFactory factory) {
-        LOGGER.info(DFSConnection.class.getName() + " is required as @Bean");
+        LOGGER.info(DFSConnection.class.getName() + " is provide as @Bean");
         return factory.getDFSConnectionWithSubDir(null);
     }
 
@@ -31,6 +39,12 @@ public class DocusafeSpringBeans {
     public SpringDFSConnectionFactory springDFSConnectionFactory(SpringDFSConnectionProperties properties) {
         LOGGER.info(SpringDFSConnectionFactory.class.getName() + " is provided as @Bean");
         return new SpringDFSConnectionFactory(properties);
+    }
+
+    @Bean
+    public DocumentSafeService documentSafeService(SpringDFSConnectionFactory dfsFactory) {
+        LOGGER.info(SpringDFSConnectionProperties.class.getName() + " is provided as @Bean");
+        return new DocumentSafeServiceImpl(dfsFactory.getDFSConnectionWithSubDir(""));
     }
 
     @Bean
--- docusafe-spring/src/test/java/de/adorsys/docusafe/spring/SpringConfigTest.java
+++ docusafe-spring/src/test/java/de/adorsys/docusafe/spring/SpringConfigTest.java
@@ -1,26 +1,37 @@
 package de.adorsys.docusafe.spring;
 
 import de.adorsys.docusafe.business.DocumentSafeService;
+import de.adorsys.docusafe.service.api.keystore.types.ReadKeyPassword;
+import de.adorsys.docusafe.service.api.types.UserID;
+import de.adorsys.docusafe.service.api.types.UserIDAuth;
 import de.adorsys.docusafe.spring.annotation.UseDocusafeSpringConfiguration;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @SpringBootTest
 @RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootConfiguration
 @UseDocusafeSpringConfiguration
-@ActiveProfiles(value="test")
 public class SpringConfigTest {
 
     @Autowired
-    public DocumentSafeService service;
+    DocumentSafeService service;
 
     @Test
-    public void a() {
+    public void createAndDestroyUser() {
         Assert.assertNotNull(service);
+        UserID userid = new UserID("peter");
+        ReadKeyPassword password = new ReadKeyPassword("password");
+        UserIDAuth userIDAuth = new UserIDAuth(userid, password);
+        assert (!service.userExists(userid));
+        service.createUser(userIDAuth);
+        assert (service.userExists(userid));
+        service.destroyUser(userIDAuth);
+
     }
 }
